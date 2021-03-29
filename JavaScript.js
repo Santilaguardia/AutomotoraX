c6f5e2e6ba5312eaa023f294319d175a9d338e35
Gaston <elgastonsantiago@gmail.com>
2021-03-28 22:17:38 -0300

    v1.1

---
+++ JavaScript.js
@@ -0,0 +1,20 @@
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    var typed = new Typed(".typing", {
+        strings: ["Ford", "Ferrari", "McLaren", "Hyundai", "Fiat"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
