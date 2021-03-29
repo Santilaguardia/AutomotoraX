c59a4bbcb95472d6a05a9a91723952dce83750a8
Gaston <elgastonsantiago@gmail.com>
2021-03-28 22:35:21 -0300

    V1.2

--- JavaScript.js
+++ JavaScript.js
@@ -1,3 +1,18 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
         $('html').css("scrollBehavior", "auto");
@@ -18,3 +33,4 @@
         backSpeed: 60,
         loop: true
     });
+});
